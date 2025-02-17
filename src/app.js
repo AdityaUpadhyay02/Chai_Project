17622874c1f0a568616d86d263878e0ccb82ff1c
Aditya Upadhyay <adityaupadhyay0220@gmail.com>
2025-02-17 09:47:06 +0530

    setup push

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ import userRouter from './routes/user.routes.js'
 
 
 //routes declaration
-app.use("/api/vi/users",userRouter)
+app.use("/api/v1/users",userRouter)
 
-
+// http://localhost:8000/api/v1/register
 export{app}
